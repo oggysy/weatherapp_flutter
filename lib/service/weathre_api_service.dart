2ae289604ec6a37e1c854a2ace397129382fc6e0
yusuke-ogiso-adglobe-co-jp <yusuke.ogiso@mail.adglobe.co.jp>
2024-05-17 17:53:18 +0900

    WeathreAPIService追加

---
+++ lib/service/weathre_api_service.dart
@@ -0,0 +1,40 @@
+import 'package:dio/dio.dart';
+import 'package:weatherapp_flutter/model/weather_response_data_model.dart';
+
+class WeathreAPIService {
+  static const WeathreAPIService instance = WeathreAPIService._internal();
+
+  const WeathreAPIService._internal();
+  factory WeathreAPIService() {
+    return instance;
+  }
+
+  final String _baseUrl = "https://api.openweathermap.org/data/2.5/forecast?r";
+  final String _apiKey = "5dfc577c1d7d94e9e23a00431582f1ac";
+  final String _units = 'metric';
+  final String _count = '8';
+  final String _lang = 'ja';
+
+  Future<WeatherResponse> fetchWeatherFromCity({required String city}) async {
+    final dio = Dio();
+    try {
+      final response = await dio.get(_baseUrl, queryParameters: {
+        'q': city,
+        'appid': _apiKey,
+        'count': _count,
+        'units': _units,
+        'lang': _lang,
+      });
+      if (response.statusCode == 200) {
+        final data = WeatherResponse.fromJson(response.data);
+        print(data);
+        return data;
+      } else {
+        throw Exception('Failed to load weather data');
+      }
+    } catch (e) {
+      print(e.toString());
+      throw Exception('Error fetching weather data: $e');
+    }
+  }
+}
